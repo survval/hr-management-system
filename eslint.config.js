87734cd11abbe4e0b29b06aa5ffbf9a83dfa8716
Suresh Babu Guvvala <suresh@pitchyou.de>
2025-07-18 14:25:14 +0200

    Fixed deployment issues at helper file

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,11 @@ module.exports = {
   root: true,
   extends: [
     'next/core-web-vitals',
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
-    // Customize rules as needed
     '@typescript-eslint/no-unused-vars': ['warn'],
     'no-console': 'warn',
     'react/react-in-jsx-scope': 'off'
